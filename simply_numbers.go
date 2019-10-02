4a8848b08b9616e8ebb46a88f262eae8be490d72
SKilliu <46762639+SKilliu@users.noreply.github.com>
2019-10-02 15:47:41 +0300

    PrimeNumFinder was added

--- simply_numbers.go
+++ simply_numbers.go
@@ -1,11 +1,7 @@
 package features
 
-import ( 
-    "fmt"
-)
-
-//This function returns true if the number is simple, and false - otherwise.
-func IsSimpleNum(number int)(result bool) {
+//This function returns true if the number is prime, and false - otherwise.
+func IsPrimeNum(number int)(result bool) {
   counter := 0
   for i := 1; i <= number; i++ {
     if number % i == 0 {
@@ -18,4 +14,13 @@ func IsSimpleNum(number int)(result bool) {
   }
 
   return false
+}
+
+//This function prints to the console primes from 1 to the specified value of the variable 'limit'.
+func PrimeNumsFinder(limit int) {
+  for i := 1; i <= limit; i++ {
+    if IsSimpleNum(i) {
+      fmt.Println(i)
+    }
+  }
 }
